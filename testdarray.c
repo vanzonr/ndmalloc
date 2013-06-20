3e0ce45dab31c887925ac92c215112452a52c115
Ramses van Zon <vanzonr@gmail.com>
2013-06-19 20:39:21 -0400

    Changed testdarray.c so it works with the reduced API

--- testdarray.c
+++ testdarray.c
@@ -1,19 +1,13 @@
-#include <stdio.h>
 #include "amalloc.h"
-#include "damalloc.h"
 
 int main()
 {
-    darray  _a = dacalloc(sizeof(double), 2, 10, 4);
-    double** a = (double**)(_a.array);
-    // ^- should become 'double a[*][*] = dmalloc(sizeof(double), 2, 10, 4);'
+    double** a = acalloc(sizeof(double), 2, 10, 4);
     a[9][3] = 7;
     double y=8;
-    daset(&y, _a, 9, 3);
+    a[9][3] = y;
     double z = a[9][3]; 
-    daget(&y, _a, 9, 3);
-    dafree(_a);
-    // ^- should become 'dfree(a);'
-    //  printf("%d\n",getpagesize());
+    y = a[9][3];
+    afree(a);
     return z+y;
 }
